9d65aba91099671499693875fc2e14e5e3dbb5b2
高橋愛奈 <dg243018@daisho.ac.jp>
2026-06-29 10:43:54 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,23 @@
 
+// アプリのファイルを一時保存（キャッシュ）する設定
+const CACHE_NAME = 'light-guide-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
